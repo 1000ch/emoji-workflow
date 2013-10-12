8148e5eb23989869f2dfe18a1dba669764cd451b
1000ch <orz1000ch@gmail.com>
2013-10-12 16:10:41 +0900

    add programs

---
+++ src/main.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require 'bundle/bundler/setup'
+require 'alfred'
+require 'lib/emoji'
+
+def generate_feedback(alfred, query)
+  feedback = alfred.feedback
+  queries  = query.split
+  emojis    = Emoji.emojis
+
+  Emoji.select!(emojis, queries)
+  emojis.each { |emoji| feedback.add_item(Emoji.item_hash(emoji)) }
+
+  puts feedback.to_alfred
+end
+
+Alfred.with_friendly_error do |alfred|
+  alfred.with_rescue_feedback = true
+  query = ARGV.join(' ').strip
+  generate_feedback(alfred, query)
+end
