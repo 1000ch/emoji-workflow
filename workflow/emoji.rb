71ba6644fb7cf339a05be041d9d7c5a335973f7b
1000ch <orz1000ch@gmail.com>
2013-10-26 23:48:11 +0900

    add comments

--- workflow/emoji.rb
+++ workflow/emoji.rb
@@ -1,28 +1,23 @@
 class Emoji
+
   def emojis
     @emojis
   end
 
+  # Emoji constructor
   def initialize(query = '')
     @emojis = load_emojis
     select!(query.split)
   end
   
+  # Load Emojis from emoji folder
   def load_emojis
     Dir.glob("./emoji/*.png").map do |path|
       File.basename(path, ".png")
     end.compact.uniq.sort
   end
   
-  def add_items(feedback, emojis = @emojis)
-    emojis.each { |emoji| feedback.add_item(item_hash(emoji)) }
-    feedback
-  end
-  
-  def to_alfred(alfred)
-    add_items(alfred.feedback).to_alfred
-  end
-
+  # Filter with arguments
   def select!(queries, emojis = @emojis)
     queries.each do |q|
       # use reject! for ruby 1.8 compatible
@@ -31,7 +26,21 @@ class Emoji
       end
     end
   end
+    
+  # Return results to Alfred
+  def to_alfred(alfred)
+    add_items(alfred.feedback).to_alfred
+  end
 
+  # Add formatted data from emojis
+  def add_items(feedback, emojis = @emojis)
+    emojis.each do |emoji|
+      feedback.add_item(item_hash(emoji))
+    end
+    feedback
+  end
+
+  # Alfred item format
   def item_hash(emoji)
     {
       :uid      => "",
